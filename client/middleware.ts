eeec30e99320006a6801bdea5a5bdccfd1e315b6
VolotDmitriy <volotkovskiy.dmitriy@gmail.com>
2026-06-03 19:41:11 +0200

    feat: add route protection middleware

---
+++ client/middleware.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export default function middleware(req: NextRequest) {
+    const token = req.cookies.get('accessToken')?.value;
+    const isAuthPage =
+        req.nextUrl.pathname.startsWith('/login') ||
+        req.nextUrl.pathname.startsWith('/register');
+
+    if (!token && !isAuthPage)
+        return NextResponse.redirect(new URL('/login', req.url));
+    if (token && isAuthPage)
+        return NextResponse.redirect(new URL('/', req.url));
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+};
