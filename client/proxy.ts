a74bea9605112074095855a1afc4be2a7f5d3385
VolotDmitriy <volotkovskiy.dmitriy@gmail.com>
2026-06-04 19:15:05 +0200

    feat: rename middleware to proxy

--- client/proxy.ts
+++ client/proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export default function middleware(req: NextRequest) {
+export default function proxy(req: NextRequest) {
     const token = req.cookies.get('accessToken')?.value;
     const isAuthPage =
         req.nextUrl.pathname.startsWith('/login') ||
